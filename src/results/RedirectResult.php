dabe86a186ea5154d6c0f82edd78592cbc6c1122
Anders Steinlein <anders@z-it.no>
2009-04-08 00:47:54 +0200

    Moved temporary storage of status message in session during redirect from RedirectResult to MessageInterceptor, matching how ValidatorInterceptor now handles temprary model storage.

--- src/results/RedirectResult.php
+++ src/results/RedirectResult.php
@@ -19,25 +19,6 @@ class RedirectResult extends AbstractResult {
 	 * Sends the redirect to the client.
 	 */
 	public function render ($request) {
-		$action = $this->getAction();
-
-		/*
-		 * If a session is active and the action has a message, store them temporarily in the
-		 * session through the redirect.
-		 */
-		if ($action instanceof SessionAware) {
-
-			if ($action instanceof MessageAware && !$action->msg->isEmpty()) {
-				$action->session['message'] = $action->msg;
-			}
-			// See ValidationInterceptor for reason this is commented away
-			//if ($action instanceof ValidationAware && !empty($action->errors)) {
-			//	$session->put('errors', $action->errors);
-			//}
-
-			$action->session->write();
-		}
-
 		header("Location: $this->location");
 		exit;
 	}
