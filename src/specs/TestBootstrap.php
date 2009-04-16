05cf94cacee36a277fd95c2689ba7d6d1e6e8562
Stian Prestholdt <stian@prestholdt.no>
2009-04-16 14:13:24 +0200

    add TestBootsrap.php to src directory and deleted the old require file.

---
+++ src/specs/TestBootstrap.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Defines the root directory of the Kolibri framework. By default this is a directory named
+ * 'kolibri' within the document root.
+ */
+if (!defined('ROOT')) {
+	if (!$rootDir = getenv('KOLIBRI_ROOT')) {
+		throw new Exception('Environment variable KOLIBRI_ROOT must be defined.');
+		exit;
+	}
+	define('ROOT', $rootDir);
+
+	/*
+	 * Defines the root directory for the application. By default this is the same directory as
+	 * this kolibri.php file.
+	 */
+	$dirname = dirname(__FILE__);
+	if (basename($dirname) == 'specs') {
+		$path = dirname(__FILE__) . '/..';
+	}
+	else $path = dirname(__FILE__) . '/../..';
+
+	define('APP_PATH', $path);
+}
+
+putenv('KOLIBRI_MODE=test');
+require(ROOT . '/core/Config.php');
+Config::getInstance();
+
+require(ROOT . '/specs/KolibriTestCase.php');
+?>
