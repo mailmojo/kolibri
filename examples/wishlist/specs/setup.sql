070f7822f707979ddfb6ef9c8ae171d0a946dc83
Stian Prestholdt <stian@prestholdt.no>
2009-04-21 12:55:34 +0200

    forgott to add a example setup file

---
+++ examples/wishlist/specs/setup.sql
@@ -0,0 +1,15 @@
+DROP TABLE items;
+
+CREATE TABLE items (
+    name TEXT PRIMARY KEY NOT NULL,
+    description TEXT DEFAULT NULL,
+    price NUMERIC DEFAULT NULL,
+    added DATE NOT NULL,
+    received DATE DEFAULT NULL
+);
+
+INSERT INTO items (name, description, price, added, received)
+VALUES('Blue Bike', 'My favorites bike in the shop', 299, '2009-04-20', '2009-04-21');
+
+INSERT INTO items (name, description, price, added, received)
+VALUES('Toy house', 'Garden toy house with several floors', 1199, '2009-04-14', '2009-04-20');
