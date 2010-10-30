1a582aac7379c742a0a36a51dc96280ca3900958
Anders Steinlein <anders@z-it.no>
2010-10-30 17:19:27 +0200

    Update IN_DB validation rule to support specifying column different from model property.

--- src/validation/InDbValidator.php
+++ src/validation/InDbValidator.php
@@ -9,12 +9,13 @@ class InDbValidator {
 	public function validate ($property, $rules) {
 		if ($this->model->$property !== null && $this->model->$property != '') {
 			$dbRelation = (isset($rules['relation']) ? $rules['relation'] : false);
+			$dbColumn = (isset($rules['column']) ? $rules['column'] : $property);
 
 			if ($dbRelation) {
 				$db = DatabaseFactory::getConnection();
 				$dbCast = (isset($rules['type']) ? '::' . $rules['type'] : '');
 
-				$query = "SELECT 1 FROM $dbRelation WHERE $property = ?{$dbCast}";
+				$query = "SELECT 1 FROM $dbRelation WHERE $dbColumn = ?{$dbCast}";
 				if (!$db->getColumn($query, $this->model->$property)) {
 					return array('in_db' => array($rules['name'], $this->model->$property));
 				}
